06add7d596f9dcf10231bdf6e4eb857e80cab1af
harsh1214 <harsh.yadav.1412@gmail.com>
2022-09-05 13:28:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const bar = document.getElementById('navbar-element');
+
+const changeNavbar = () => {
+    let screenSize = screen.width;
+    if(screenSize >= 662){
+        bar.classList.remove('navbar-add');
+    }
+}
+window.addEventListener("resize", changeNavbar);
+changeNavbar();
+
+const navbarSwitch = () => {
+    if(bar.classList[1] === 'navbar-add'){
+        bar.classList.remove('navbar-add');
+    }
+    else{
+        bar.classList.add('navbar-add');
+    }
+}
